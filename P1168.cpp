cf84567edabea58b52011049c8f2954c9c369544
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2024-10-25 15:54:24 +0800

    P1168

--- P1168.cpp
+++ P1168.cpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author       : FeiYehua
  * @Date         : 2024-10-25 15:23:22
- * @LastEditTime : 2024-10-25 15:45:37
+ * @LastEditTime : 2024-10-25 15:52:04
  * @LastEditors  : FeiYehua
  * @Description  : 
  * @FilePath     : P1168.cpp
@@ -29,3 +29,10 @@ int main()
     }
     return 0;
 }
+/*
+vector的使用：
+类似于数组的元素访问；
+访问begin，end方法可以返回迭代器，用于与其他STL配合使用；
+insert方法：第一个参数为一个迭代器，第二个参数为数量n，第三个参数为值；
+在该迭代器前插入n个对应数值
+*/
