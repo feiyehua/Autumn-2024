c8eecabe1f7ba8f9e04a28292f447af34e74376c
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2024-11-09 17:41:49 +0800

    kingdom 55pts

--- MX-S5/kingdom.cpp
+++ MX-S5/kingdom.cpp
@@ -1,23 +1,23 @@
 /*** 
  * @Author       : FeiYehua
  * @Date         : 2024-11-09 08:38:54
- * @LastEditTime : 2024-11-09 13:01:22
+ * @LastEditTime : 2024-11-09 17:39:24
  * @LastEditors  : FeiYehua
  * @Description  : 
- * @FilePath     : T536792.cpp
+ * @FilePath     : kingdom.cpp
  * @     © 2024 FeiYehua
  */
 #include<bits/stdc++.h>
 using namespace std;
 const long long MOD=1e9+7;
 int n;//s的长度
-int m;//看到后m个字符
+long long m;//看到后m个字符
 int q;//q组测试
 const int maxN=2e5+10;
 bool str[maxN*2];
 long long s,k;
 int pri[maxN*2];//记录上一个1出现的位置
-int nextLoc[20*2];
+int nextLoc[maxN*2];
 bool isAll0=1,isAll1=1;
 int length[maxN];
 bool vis[maxN];
@@ -57,7 +57,10 @@ void getNextLoc()//记录每个点开始、到下一个点是什么
 }
 int main()
 {
-    freopen("/Users/xiong/Documents/CPP/Autumn-2024/MX-S5/kingdom/kingdom3.in","r",stdin);
+    #ifndef ONLINE_JUDGE
+    freopen("/Users/xiong/Documents/CPP/Autumn-2024/MX-S5/kingdom/kingdom5.in","r",stdin);
+    freopen("/Users/xiong/Documents/CPP/Autumn-2024/MX-S5/kingdom/ans.out","w",stdout);
+    #endif
     n=fr(),m=fr(),q=fr();
     int lastOne=-1;
     for(int i=1;i<=n;i++)
@@ -82,6 +85,7 @@ int main()
     for(int i=1;i<=q;i++)
     {
         s=fr(),k=fr();
+        //if(i<=4) continue;
         if (isAll0)
         {
             // 每次前移会移到i+1上
@@ -91,28 +95,37 @@ int main()
         if(isAll1)
         {
             //每次都会移动到i+m上
+            //long long a1=s%MOD,a2=k%MOD,a3=m%MOD,a4=(a2*a3)%MOD,a5=(a1+a4)%MOD;
             printf("%lld\n",(s+(k%MOD)*(m%MOD))%MOD);
+            continue;
         }
         if(k==0) printf("%lld\n",s%MOD);
         else
         {
             long long ans=s;
-            long long start=s;
-            long long b=(m/n)*n;
-            b%=MOD;
+            ans%=MOD;
+            long long start=s%n;
+            s%=n;
+            if(s==0) s+=n;
+            if(start==0) start+=n;
+            long long b=((m/n)%MOD*n%MOD)%MOD;
             for (long long j = 1; j <= k; j++)
             {
                 ans=ans+b+length[s];
                 ans%=MOD;
+                //cout<<i<<" "<<j<<" "<<ans<<" "<<b<<" "<<length[s]<<endl;
                 s=nextLoc[s];
                 if(s==start) 
                 {
                     //前j次为一个循环
-                    ans=(((ans-start)%MOD)*((k/j-1)%MOD)%MOD)+start;
-                    j=0,k=k%j;
+                    //cout<<j<<endl;
+                    ans=(((ans-start)%MOD)*((k/j)%MOD)%MOD)+start;
+                    //cout<<s<<" "<<ans<<endl;
+                    k=k%j,j=0;
                 }
             }
             printf("%lld\n",ans);
         }
     }
+    return 0;
 }
