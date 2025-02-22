2e93a9091b40816f56f6172ecaaf776ef71699a5
FeiYehua <31472675+feiyehua@users.noreply.github.com>
2025-02-22 10:44:15 +0800

    P11268 buy

--- MX-S5/buy.cpp
+++ MX-S5/buy.cpp
@@ -1,194 +1,113 @@
-/*** 
+/***
  * @Author       : FeiYehua
  * @Date         : 2024-11-09 09:03:54
- * @LastEditTime : 2024-11-09 20:08:40
+ * @LastEditTime : 2025-02-22 10:38:28
  * @LastEditors  : FeiYehua
- * @Description  : 
+ * @Description  :
  * @FilePath     : buy.cpp
  * @     © 2024 FeiYehua
  */
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define int long long
-//从优惠金额a-b最小的看起，找到优惠金额最大的优惠券
-//用a-b排序物品，优惠起价w排序优惠券，区间查找最大的v
-//使用线段树维护区间最大值
-//找到最大优惠额，用总价减去即可
-int n,m;
-//命题人介绍了反悔贪心；
-//同样需要考虑用券给总价格带来的贡献：disc-v，使贡献值达到最大。
-//比如，同样是5 4的优惠券，
-//有5 3
+// 从优惠金额a-b最小的看起，找到优惠金额最大的优惠券
+// 用a-b排序物品，优惠起价w排序优惠券，区间查找最大的v
+// 使用线段树维护区间最大值
+// 找到最大优惠额，用总价减去即可
+int n, m;
+// 命题人介绍了反悔贪心；
+// 同样需要考虑用券给总价格带来的贡献：disc-v，使贡献值达到最大。
+// 比如，同样是5 4的优惠券，
+// 有5 3
 //
-int maxDis,sum;
-const int maxN=1e6+10;
-int minA=maxN,maxW=0;
+int maxDis, sum;
+const int maxN = 1e6 + 10;
+int minA = maxN, maxW = 0;
 long long fr()
 {
     char ch;
-    long long num=0;
-    ch=getchar();
-    while(isspace(ch)) ch=getchar();
-    while(isdigit(ch))
+    long long num = 0LL;
+    ch = getchar();
+    while (isspace(ch))
+        ch = getchar();
+    while (isdigit(ch))
     {
-        num=num*10+ch-'0';
-        ch=getchar();
+        num = num * 10LL + ch - '0';
+        ch = getchar();
     }
     return num;
 }
-struct item{
-    int a,b,disc;
-}items[maxN];
-struct cop{
-    int w,v;//w起用
-}cops[maxN];
-struct tree{
-    int l;
-    int r;
-    int maxDis;
-    int loc;//取到最大值的时刻
-}nodes[maxN*4];
-int a[2*maxN];
-int loc;
-int leafLoc[maxN];
-void updateMaxLoc(int point)
+struct item
 {
-    if(nodes[point*2].maxDis>nodes[point*2+1].maxDis)
+    int a, b, disc;
+} items[maxN];
+struct cop
+{
+    int w, v; // w起用
+    bool operator<(const cop &t) const
     {
-        nodes[point].maxDis=nodes[point*2].maxDis;
-        nodes[point].loc=nodes[point*2].loc;
+        return v < t.v;
     }
-    else
+    bool operator>(const cop &t) const
     {
-        nodes[point].maxDis=nodes[point*2+1].maxDis;
-        nodes[point].loc=nodes[point*2+1].loc;
+        return v > t.v;
     }
+} cops[maxN + maxN];
+priority_queue<cop, vector<cop>, less<cop>> q;
+bool cmpI(const item &a, const item &b)
+{
+    // if(a.disc!=b.disc)
+    // return a.disc<b.disc;
+    // else
+    return a.a < b.a;
 }
-// int findLoc(int w)
-// {
-//     w++;//找第一个小于等于w+1的下标
-//     int l=1,r=n;
-//     int mid=(l+r)/2;
-//     while(l<r)
-//     {
-//         if(cops[mid].w<w) l=mid;
-//         else if(cops[mid].w>=w) r=mid-1;
-//     }
-// }
-void build(int l,int r,int point)
+bool cmpC(const cop &a, const cop &b)
 {
-    nodes[point].l=l,nodes[point].r=r;
-    if(l==r)
-    {
-        nodes[point].maxDis=cops[l].v;
-        nodes[point].loc=l;
-        leafLoc[l]=point;
-        return;
-    }
-    else
-    {
-        int mid=(l+r)/2;
-        build(l,mid,point*2);
-        build(mid+1,r,point*2+1);
-        //nodes[point].maxDis=max(nodes[point*2].maxDis,nodes[point*2+1].maxDis);
-        updateMaxLoc(point);
-    }
-}
-int getMax(int l,int r,int point,int tl,int tr,int* loc)
-{
-    if(r<tl||l>tr) return -INT_MAX;
-    if(r<=tr&&l>=tl) {*loc=nodes[point].loc;return nodes[point].maxDis;}
-    int mid=(l+r)/2;
-    int loc1,loc2;
-    int max1=getMax(l,mid,point*2,tl,tr,&loc1),max2=getMax(mid+1,r,point*2+1,tl,tr,&loc2);
-    if(max1<max2)
-    {
-        *loc=loc2;
-        return max2;
-    }
-    else
-    {
-        *loc=loc1;
-        return max1;
-    }
-}
-void update(int point)//这个点的券被用了
-{
-    nodes[point].maxDis=-INT_MAX;
-    point/=2;
-    while(point>=1)
-    {
-        updateMaxLoc(point);
-        //nodes[point].maxDis=max(nodes[point*2].maxDis,nodes[point*2+1].maxDis);
-        point/=2;
-    }
-}
-bool cmpI(const item& a,const item& b)
-{
-    if(a.disc!=b.disc)
-    return a.disc<b.disc;
-    else return a.a<b.a;
-}
-bool cmpC(const cop& a,const cop& b)
-{
-    return a.w<b.w;
+    return a.w < b.w;
 }
 signed main()
 {
-    #ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE
 
-    freopen("./Autumn-2024/MX-S5/buy/buy6.in","r",stdin);
-    freopen("./Autumn-2024/MX-S5/buy/ans.out","w",stdout);
-    #endif
-    n=fr(),m=fr();
-    for(int i=1;i<=n;i++)
+    freopen("/Users/xiong/Documents/CPP/Autumn-2024/MX-S5/buy/buy6.in", "r", stdin);
+    freopen("/Users/xiong/Documents/CPP/Autumn-2024/MX-S5/buy/ans.out", "w", stdout);
+#endif
+    // cin>>n>>m;
+    n = fr(), m = fr();
+    for (int i = 1; i <= n; i++)
     {
-        items[i].a=fr(),items[i].b=fr();
-        items[i].disc=items[i].a-items[i].b;
-        sum+=items[i].a;
-        minA=min(minA,items[i].a);
+        items[i].a = fr(), items[i].b = fr();
+        items[i].disc = items[i].a - items[i].b;
+        sum += items[i].a;
     }
-    for(int i=1;i<=m;i++)
+    for (int i = 1; i <= m; i++)
     {
-        cops[i].w=fr(),cops[i].v=fr();
-        maxW=max(maxW,cops[i].w);
+        cops[i].w = fr(), cops[i].v = fr();
     }
-    if(maxW<=minA)
+    for (int i = m + 1; i <= m + n; i++)
     {
-        for(int i=1;i<=n;i++)
-        {
-            a[i]=items[i].disc;
-        }
-        for(int i=1;i<=m;i++)
-        {
-            a[i+n]=cops[i].v;
-        }
-        sort(a+1,a+1+m+n,greater<int>());
-        for(int i=1;i<=n;i++)
-        {
-            sum-=a[i];
-        }
-        cout<<sum;
-        return 0;
+        cops[i].w = items[i - m].a;
+        cops[i].v = items[i - m].disc;
     }
-    sort(items+1,items+1+n,cmpI);
-    sort(cops+1,cops+1+m,cmpC);
-    build(1,m,1);
-    for(int i=1;i<=n;i++)
+    sort(items + 1, items + 1 + n, cmpI);   // 对所有物品排序
+    sort(cops + 1, cops + 1 + m + n, cmpC); // 对所有优惠券排序
+    int cnt = 1;
+    for (int i = 1; i <= n; i++)
     {
-        //对第i个物品用券
-        cop tmp;
-        tmp.w=items[i].a+1;
-        int r=lower_bound(cops+1,cops+1+m,tmp,cmpC)-cops-1;
-        loc=-1;
-        int curMax=getMax(1,m,1,1,r,&loc);
-        //cout<<r<<" "<<i<<" "<<curMax<<" "<<maxDis<<endl;
-        maxDis+=max(curMax,items[i].disc);
-        if(curMax>items[i].disc)
+        // 对第i个物品用券
+        while (cnt <= m + n && items[i].a >= cops[cnt].w)
         {
-            update(leafLoc[loc]);
+            q.push(cops[cnt]);
+            cnt++;
+        }
+        if (!q.empty())
+        {
+            int curMax = q.top().v;
+            q.pop();
+            maxDis += curMax;
         }
     }
-    cout<<sum-maxDis<<endl;
+    // assert(sum>0);
+    cout << (sum - maxDis) << endl;
     return 0;
 }
